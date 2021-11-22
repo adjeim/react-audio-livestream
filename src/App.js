aea3d2eec32afcb15cf9bd38bc14d849fa4e9837
Mia Adjei <adjei.m@gmail.com>
2021-11-22 11:11:44 -0500

    Add components for streamer and audience experience

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import { Theme } from '@twilio-paste/core/theme';
+import { Box } from '@twilio-paste/core/box';
+import { Grid, Column } from '@twilio-paste/core/grid';
+import { Label } from '@twilio-paste/core/label';
+import { Input } from '@twilio-paste/core/input';
+import { Button } from '@twilio-paste/core/button';
+import { Card } from '@twilio-paste/core/card';
+import { Heading } from '@twilio-paste/core/heading';
+
+import Dashboard from './Dashboard';
+
+const App = () => {
+  const [username, setUsername] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUsername(event.target.username.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Theme.Provider theme='default'>
+      <Grid gutter='space30'>
+        <Column span={10} offset={1}>
+          {username ?
+            <Dashboard username={username} setUsername={setUsername} />
+          :
+          <Box margin='space100'>
+            <Card padding="space100">
+              <form onSubmit={handleSubmit}>
+                <Heading as='h2' variant='heading30'>Sign In</Heading>
+                <Box marginBottom='space30'>
+                  <Label htmlFor='username' required>Username</Label>
+                  <Input
+                    aria-describedby='username_help_text'
+                    id='username'
+                    name='username'
+                    type='text'
+                    placeholder='username123'
+                    required/>
+                </Box>
+                <Button variant='primary' type='submit'>Sign In</Button>
+              </form>
+            </Card>
+          </Box>
+          }
+        </Column>
+      </Grid>
+    </Theme.Provider>
+  )
+};
 
 export default App;
